ff8d47859d93e0c675ff902d4acf0568c0a44e2f
Veronica Bustabad <vbustabad@gmail.com>
2018-07-01 16:03:00 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -15,7 +15,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     self.artists.collect do |artist|
       artist.name
-    end 
+    end
     # return an array of strings containing every musician's name
   end
 end
